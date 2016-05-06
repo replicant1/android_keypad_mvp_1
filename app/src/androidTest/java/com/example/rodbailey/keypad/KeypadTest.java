c9a182dbcf4a3f660f7929d2f111a29cde270926
rodbailey <rodney.p.bailey@gmail.com>
2016-05-06 21:24:12 +1000

    More Realm tests

--- app/src/androidTest/java/com/example/rodbailey/keypad/KeypadTest.java
+++ app/src/androidTest/java/com/example/rodbailey/keypad/KeypadTest.java
@@ -10,12 +10,18 @@ import android.test.suitebuilder.annotation.LargeTest;
 
 import com.example.rodbailey.keypad.pinservice.Pin;
 
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -29,34 +35,72 @@ public class KeypadTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
-    @Test
-    public void testCreateRealmDatabase() {
+    public RealmConfiguration config;
+
+    public Realm realm;
+
+    @Before
+    public void before() {
+        config = new RealmConfiguration.Builder(mActivityRule.getActivity()).build();
+        realm = Realm.getInstance(config);
+        emptyRealmDatabase();
+    }
+
+    @After
+    public void after() {
+        realm.close();
+    }
+
+    private void emptyRealmDatabase() {
+        realm.beginTransaction();
+        RealmResults<Pin> allPins = realm.where(Pin.class).findAll();
+        allPins.deleteAllFromRealm();
+        realm.commitTransaction();
+    }
+
+    private void addPinToRealmDatabase() {
         // Default realm database is /data/user/0/com.example.rodbailey.keypad/files/default.realm
         // Due to symbolic links, /data/data/com.example.rodbailey.keypad/files/default.realm
-        Log.log(TAG, "--------------------");
-        Log.log(TAG, "Realm absolute database directory is: " + mActivityRule.getActivity().getFilesDir().getAbsolutePath());
-
-        RealmConfiguration config = new RealmConfiguration.Builder(mActivityRule.getActivity()).build();
-        Realm realm = Realm.getInstance(config);
-
-        Log.log(TAG, "Performing CREATE for a Pin John Smith, 1234");
-
         realm.beginTransaction();
         Pin pin = realm.createObject(Pin.class);
         pin.setId(1);
         pin.setName("John Smith");
         pin.setNumericCode("1234");
         realm.commitTransaction();
+    }
 
-
-        Log.log(TAG, "Created new Pin: 1,John Smith,1234");
+    @Test
+    public void testReadRealmDatabase() {
+        addPinToRealmDatabase();
 
         Pin retrievedPin = realm.where(Pin.class).findFirst();
+        Assert.assertNotNull(retrievedPin);
+        Assert.assertEquals(retrievedPin.getId(), 1);
+        Assert.assertEquals(retrievedPin.getName(), "John Smith");
+        Assert.assertEquals(retrievedPin.getNumericCode(), "1234");
         Log.log(TAG, "retrieved Pin=" + retrievedPin);
-        Log.log(TAG, "--------------------");
+    }
 
+    @Test
+    public void testQueryRealmDatabase() {
+        addPinToRealmDatabase();
+        
+        RealmQuery<Pin> query = realm.where(Pin.class);
+        query.equalTo("name", "John Smith");
+        RealmResults<Pin> results = query.findAll();
+        Assert.assertNotNull(results);
+        Assert.assertEquals(1, results.size());
 
-        realm.close();
+        Pin queryPin = results.first();
+        Assert.assertEquals(queryPin.getId(), 1);
+        Assert.assertEquals(queryPin.getName(), "John Smith");
+        Assert.assertEquals(queryPin.getNumericCode(), "1234");
+        Log.log(TAG, "queried Pin=" + queryPin);
+    }
+
+    @Test
+    public void testComplexQueryRealmDatabase() {
+
     }
 
     /*
